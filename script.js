646305a24f665063e61df1e0bdbc1f7e5b1dbc99
Cergey <sguest@mail.ru>
2020-11-21 01:16:24 +0300

    Chat-complited

--- script.js
+++ script.js
@@ -1,10 +1,68 @@
-const userForm = document.querySelector("#user_form")
+// tpSFqiCtPEJPqDxyOvQ3
 
 const forum = document.querySelector('#forum')
 const alertMsg = document.querySelector('.alert')
 
-console.log(userForm)
-console.dir(userForm)
+const getMessagesBtn = document.getElementById('get-chat-messages')
+
+fetch('http://user08.test1.seschool.ru:3000/api/chat/').then((response)=> {
+    if (response.ok) {
+            return response.json()
+        }
+}).then((messages) => {
+    let resaltHtml = ''
+                messages.forEach(message => {
+                resaltHtml += `<li class="list-group-item">${message.username}: ${message.message}
+                </li>`
+        });
+        forum.innerHTML = resaltHtml
+})
+const evtSource = new EventSource("http://user08.test1.seschool.ru:3000/api/chat/subscribe");
+evtSource.onmessage = function (event) {
+    const newElement = document.createElement("li");
+    newElement.classList.add("list-group-item")
+    const data = JSON.parse(event.data)
+    newElement.innerHTML = `${data.username}: ${data.message}`
+    forum.insertBefore(newElement, forum.firstElementChild);
+}
+
+
+const postMessagesBtn = document.getElementById('post-chat-message')
+const userForm = document.querySelector("#user_form")
+// postMessagesBtn.addEventListener('click', () => {
+const postMessage = () => {
+    const username = userForm[0].value //получили значение инпута
+    const email = userForm[1].value
+    const message = userForm[2].value
+    fetch('http://user08.test1.seschool.ru:3000/api/chat/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'bearer TOKEN'
+        },
+        body: JSON.stringify({            
+                username,
+                email,
+                message: message            
+        })
+    }).then(response => {  
+        if(response.ok){
+            return response.json()
+        }   else {
+            throw new Error(`Status: ${response.status}. Message: ${response.statusText}`)
+        }   
+                        
+    }).then(response => {
+        alertMsg.classList.remove('show')
+        alertMsg.textContent = ''
+        console.log(response)
+    }).catch(err => {
+        console.error(err)
+        alertMsg.classList.add('show')
+        alertMsg.textContent = err
+    })
+}
+
 
 userForm.addEventListener('submit', function(event) {
     console.log(event)
@@ -20,26 +78,27 @@ userForm.addEventListener('submit', function(event) {
       alertMsg.classList.add('show')
         return
     }
+    postMessage ();
 
-    alertMsg.innerHTML = ''
-    //alertMsg.style.display = 'none'
-    alertMsg.classList.remove('show')
+    // alertMsg.innerHTML = ''
+    // //alertMsg.style.display = 'none'
+    // alertMsg.classList.remove('show')
 
-    const userMessage = {
-        userName: formData.get('user_name'),
-        userEmail: formData.get('user_email'),
-        userMessage: formData.get('user_message')
-    }
+    // const userMessage = {
+    //     userName: formData.get('user_name'),
+    //     userEmail: formData.get('user_email'),
+    //     userMessage: formData.get('user_message')
+    // }
    
-    userForm[0].value = ''
-    userForm[1].value = ''
-    userForm[2].value = ''
+    // userForm[0].value = ''
+    // userForm[1].value = ''
+    // userForm[2].value = ''
     
     
-    forum.insertAdjacentHTML("afterbegin", `<li class="list-group-item">         
-            <span>${userMessage.userName}: </span>
-             <span>${userMessage.userMessage} </span>         
-     </li>`)
+    // forum.insertAdjacentHTML("afterbegin", `<li class="list-group-item">         
+    //         <span>${userMessage.userName}: </span>
+    //          <span>${userMessage.userMessage} </span>         
+    //  </li>`)
     
 })
 
